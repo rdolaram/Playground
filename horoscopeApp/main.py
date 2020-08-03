e430d8efa443684f7a7a316be79ee54dadf9a499
RakeshDolaram <dolaramrakesh@gmail.com>
2020-08-02 21:48:58 -0500

    added webscraping horoscope application

---
+++ horoscopeApp/main.py
@@ -0,0 +1,94 @@
+'''
+Rakesh Dolaram
+Displays daily horoscope & quote.
+'''
+
+from PyQt5.QtWidgets import *
+from PyQt5 import QtGui, QtCore
+from PyQt5.QtGui import QFont
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        
+        self.initUI()
+        self.getHoroscope()
+        
+        vbox = QVBoxLayout()
+        
+        vbox.addWidget(self.combo)
+        vbox.addWidget(self.label)
+  
+        self.setLayout(vbox)
+
+        self.show()
+
+    def initUI(self):
+        self.setWindowTitle("Horoscopes")
+        self.setFixedWidth(1000)
+        self.setFixedHeight(300)
+        
+        self.label = QLabel()
+        self.setFont(QtGui.QFont("Times", 15, QFont.Bold))
+        self.label.setWordWrap(True)
+
+        self.combo = QComboBox()
+
+        self.combo.addItem("Aries")
+        self.combo.addItem("Taurus")
+        self.combo.addItem("Gemini")
+        self.combo.addItem("Cancer")
+        self.combo.addItem("Leo")
+        self.combo.addItem("Virgo")
+        self.combo.addItem("Libra")
+        self.combo.addItem("Scorpio")
+        self.combo.addItem("Sagittarius")
+        self.combo.addItem("Capricorn")
+        self.combo.addItem("Aquarius")
+        self.combo.addItem("Pisces")
+
+        self.combo.currentTextChanged.connect(self.getHoroscope)
+
+    def getHoroscope(self):
+        text = self.combo.currentText()
+        
+        if text == 'Aries':
+            number = '1'
+        elif text == "Taurus":
+            number = '2'
+        elif text == "Gemini":
+            number = '3'
+        elif text == "Cancer":
+            number = '4'
+        elif text == "Leo":
+            number = '5'
+        elif text == "Virgo":
+            number = '6'
+        elif text == "Libra":
+            number = '7'
+        elif text == "Scorpio":
+            number = '8'
+        elif text == "Sagittarius":
+            number = '9'
+        elif text == "Capricorn":
+            number = '10'
+        elif text == "Aquarius":
+            number = '11'
+        elif text == "Pisces":
+            number = '12'
+        
+        result = requests.get("https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-today.aspx?sign="+ number)
+        src = result.content
+        soup = BeautifulSoup(src)
+        page = soup.find("p").getText()
+        self.label.setText(page + "\n\nHoroscope from: https://www.horoscope.com/us/index.aspx")
+                
+
+if __name__ == '__main__':
+    App = QApplication(sys.argv)
+    App.setStyle('Fusion')
+    window = Window()
+    sys.exit(App.exec())
