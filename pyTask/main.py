a6d2c27f14fa1e1ea5d60e5c3be99ae1ac9fc5e7
RakeshDolaram <dolaramrakesh@gmail.com>
2020-08-07 16:50:04 -0500

    added minimal features

--- pyTask/main.py
+++ pyTask/main.py
@@ -5,57 +5,100 @@ To do application
 
 from PyQt5.QtWidgets import *
 from PyQt5 import QtGui, QtCore
+import random
 import sys
+
+#CODE TO IMPLEMENT
+#WHEN WE CLICK A TASK (QLISTWIDGET OBJECT)
+# WE ARE BROUGHT WITH A MENU OPTION SUCH AS RENAME
+# NOTES
+# ETC
+
 
 class Window(QWidget):
     def __init__(self):
         super().__init__()
 
+        self.dialogFont = QtGui.QFont("Arial", 15, QtGui.QFont.Bold)
+        self.font = QtGui.QFont("Arial", 15)
+        
         self.initUI()
 
 
     def initUI(self):
-        self.setGeometry(300,300,500,300)
+        self.setGeometry(1000,500,500,300)
         self.setWindowTitle("To Do Application")
 
         #INPUT TASK
         self.lineEdit = QLineEdit(self)
         self.lineEdit.returnPressed.connect(self.enterPressed)
 
-        #ADD BUTTON
-        button = QPushButton("Add task", self)
-        button.clicked.connect(self.buttonClicked)
+        #BUTTONS
+        addButton = QPushButton("Add", self)
+        addButton.clicked.connect(self.addTask)
+
+        removeButton = QPushButton("Remove", self)
+        removeButton.clicked.connect(self.removeTask)
 
         #LIST
-        self.list = QListWidget()
+        self.list = []
+        self.listWidget = QListWidget()
 
 
         #LAYOUT MANAGEMENT        
         hbox = QHBoxLayout()
         hbox.addWidget(self.lineEdit)
-        hbox.addWidget(button)
+        hbox.addWidget(addButton)
+        hbox.addWidget(removeButton)
 
         vbox = QVBoxLayout()
         vbox.addLayout(hbox)
-        vbox.addWidget(self.list)
+        vbox.addWidget(self.listWidget)
         self.setLayout(vbox)
-
 
         self.show()
 
-    def buttonClicked(self):
+    def addTask(self):
         text = self.lineEdit.text()
-        self.list.addItem(text)
-    
-    #DEBATING ON WHETHER ENTER FUNCTIONALITY WILL BE IMPLEMENTED.
-    '''
+        if text in self.list:
+            self.duplicateError()
+        else:
+            self.list.append(text)
+            #CREATE THE ITEM
+            x = QListWidgetItem()
+            x.setText(text)
+            x.setFont(self.font)
+            x.setTextAlignment(QtCore.Qt.AlignHCenter)
+            
+            self.listWidget.addItem(x)
+
+    def removeTask(self):
+        selected = self.listWidget.selectedItems()
+        if not selected: return
+        for item in selected:
+            self.listWidget.takeItem(self.listWidget.row(item))
+
     def enterPressed(self):
         text = self.lineEdit.text()
-        self.list.addItem(text)
-    '''
+        self.listWidget.addItem(text)
 
+    def duplicateError(self):
+        dialog = QDialog()
 
+        duplicateLabel = QLabel("Duplicate task", dialog)
+        duplicateLabel.setAlignment(QtCore.Qt.AlignCenter)
+        duplicateLabel.setFont(self.dialogFont)
 
+        dialog.setWindowTitle("WARNING")
+        dialog.setWindowModality(QtCore.Qt.ApplicationModal)
+        dialog.setFixedHeight(100)
+        dialog.setFixedWidth(250)
+
+        vbox = QVBoxLayout()        
+        vbox.addWidget(duplicateLabel)
+    
+        dialog.setLayout(vbox)
+        dialog.exec_()
 
 #START THE APPLICATION
 if __name__ == '__main__':
